b4329dbbf289d02c296eb2229dcffb71053e0309
Vainseem <2311218443@qq.com>
2025-04-03 19:23:27 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '金融APP',
+  webDir: 'dist'
+};
+
+export default config;
